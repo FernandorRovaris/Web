03bde03aba66352cf1d3ae3f4bc5d885afcfb98c
FernandorRovaris <54208673+FernandorRovaris@users.noreply.github.com>
2021-01-07 21:36:30 -0300

    Create objeto2.js

---
+++ exercicios-js/Fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const clinte = function() {}
+console.log(typeof clinte)
+console.log(typeof new clinte)
+
+class Produto{}
+console.log(typeof Produto)
+console.log(typeof new Produto)
