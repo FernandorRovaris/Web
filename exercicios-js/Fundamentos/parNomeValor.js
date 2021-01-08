4054d410e3a193a3b2b3bdf2ce72f8d80736fa72
FernandorRovaris <54208673+FernandorRovaris@users.noreply.github.com>
2021-01-07 21:44:25 -0300

    Create parNomeValor.js

---
+++ exercicios-js/Fundamentos/parNomeValor.js
@@ -0,0 +1,22 @@
+// par nome/valor
+const saudacao = 'Opa'
+
+
+function exec(){
+    const saudacao = 'Falaaa'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90.0,
+    endereco:{
+        logradouro: 'Rua Muito legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao),
+console.log(exec())
+console.log(cliente)
