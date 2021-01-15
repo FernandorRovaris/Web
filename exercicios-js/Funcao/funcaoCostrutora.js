2104e7931c70d1fcec186e44c9ba6c53c054d722
FernandorRovaris <54208673+FernandorRovaris@users.noreply.github.com>
2021-01-14 21:12:27 -0300

    Create funcaoCostrutora.js

---
+++ exercicios-js/Funcao/funcaoCostrutora.js
@@ -0,0 +1,30 @@
+function Carro(velocidadeMaxima = 200, delta = 5){
+    // Atributo privado
+    let velocidadeAtual = 0;
+    
+    // metodo publico
+    this.acelerar = function () {
+        if((velocidadeAtual + delta) <= velocidadeMaxima){
+            velocidadeAtual +=  delta;
+        }else{
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    //metodo publico
+    this.velocidadeAtual =  function (){
+        return velocidadeAtual
+    }
+
+}
+
+const uno = new Carro;
+uno.acelerar();
+console.log(uno.velocidadeAtual());
+
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.velocidadeAtual());
