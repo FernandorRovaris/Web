99c2ccfbce2ca5d08239a9c83020fcabadeeaa78
Fernando Rovaris <FernandorRovaris@users.noreply.github.com>
2021-03-01 11:48:00 -0300

    Node Aula 1

---
+++ node/moduleB.js
@@ -0,0 +1,6 @@
+module.exports = {
+    bomDia: 'Boa Dia',
+    boaNoite(){
+        return 'Boa Noite'
+    }
+}
---
+++ node/moduloA.js
@@ -0,0 +1,5 @@
+ this.ola = 'Fala Pessoal'
+
+ exports.bemVindo = 'Bem vindoao Node!'
+
+ module.exports.ateLogo = 'Ate o preoximo exemplo'
