390645a5529d16dd451041f6d3d818f471293d4c
Fernando Rovaris <FernandorRovaris@users.noreply.github.com>
2021-03-03 09:42:30 -0300

    projeto

--- node/Projeto/src/servidor.js
+++ node/Projeto/src/servidor.js
@@ -2,14 +2,41 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const boryParser = require('body-parser')
+const bancoDeDados = require('./BancoDeDados')
+
+app.use(boryParser.urlencoded({extended: true}))
 
 app.get('/produtos',(req, res, next) => {
-    console.log('Middleware 1...');
-    next()
+   // res.send({nome: 'Notebook', preco: 1233.45})
+   res.send(bancoDeDados.getProdutos())
 })
 
-app.get('/produtos',(req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45})
+app.get('/produtos/:id',(req,res,next)=>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos',(req, res, next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id',(req, res, next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+
+app.delete('/produtos/:id',(req, res, next)=>{
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, ()=>{
