050cfd46346eea0c0d8adcdf3b50f34ec64291bd
Fernando Rovaris <54208673+FernandorRovaris@users.noreply.github.com>
2021-03-13 01:31:15 -0300

    aula 3 formulario

--- exercicios-js/html/exercicios/form/server.js
+++ exercicios-js/html/exercicios/form/server.js
@@ -6,7 +6,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios',(req,res)=>{
     console.log(req.body);
-    res.send('<h1>Parabéns</h1>')
+    res.send('<h1>Parabéns. Usuario Incluido</h1>')
 })
+
+app.post('/usuarios/:id',(req,res)=>{
+    console.log(req.params.id);
+    console.log(req.body);
+    res.send('<h1>Parabéns. Usuario Alterado </h1>')
+})
+
 
 app.listen(3003)
